d32f44ca90d91e14b3aa1e8e31b569d1124fe4d9
Priangshu Das <88083475+priangshudas@users.noreply.github.com>
2023-10-13 01:41:36 +0530

    backfround image hover and  changing animation on hover and right click

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-Shery.mouseFollower({
-    //Parameters are optional.
-    // skew: true,
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    duration: 1,
+// Shery.mouseFollower({
+//     //Parameters are optional.
+//     // skew: true,
+//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//     duration: 1,
+//   });
+
+  Shery.imageEffect("#back", {
+    Style: 5,
+    config: {"a":{"value":0.74,"range":[0,30]},"b":{"value":-0.95,"range":[-1,1]},"aspect":{"value":2.1996779388083736},"gooey":{"value":true},"infiniteGooey":{"value":true},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":false},"onMouse":{"value":1},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.15,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+    // debug: true,
+    gooey: true,
   });
